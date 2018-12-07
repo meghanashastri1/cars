c39dcf0bb81763e7fcf541c313fee49883d4e246
meghanashastri1 <meghana360@gmail.com>
2018-12-06 21:53:38 -0800

    update

--- cars.sh
+++ cars.sh
@@ -2,19 +2,31 @@
 #cars.sh
 #Meghana Shastri
 
-echo "Enter a year, make, and model for each car you want to add to your car list"
-read input
+run=true
 
-while [$input -ne "quit"]
-do 
-	echo "year:"
-	read year
-	echo "make:"
-	read make
-	echo "model:"
-	read model
+while $run
+do
+	echo "type 'add' to add a car into the list, type 'list' to list the cars in the list, or type 'quit' to quit"
+	read input
+	case $input in
+	"add")
+		echo "year:"
+		read year
+		echo "make:"
+		read make
+		echo "model:"
+		read model
 
-	echo "$year:$make:$mode" >> My_old_cars
-	$sort My_old_cars
-	
-	echo "Would you like to list the contents of My_old_cars?"
+		car=$year":"$make":"$model
+		echo $car >> My_old_cars
+		;;
+
+	"list")
+		sort -n My_old_cars
+		;;
+	"quit")
+		run=false
+		;;
+	esac
+done
+echo "goodbye"
